4a1b064a6b4af5d5335da0d18362e245f67fd374
Igor <igorgsbarbosadev@gmail.com>
2026-07-25 09:52:39 -0300

    Remove NoOpAuditAdapter (replaced by PersistentAuditAdapter)

--- src/main/java/dev/igorbarbosa/worktrainingsystem/identity/application/NoOpAuditAdapter.java
+++ src/main/java/dev/igorbarbosa/worktrainingsystem/identity/application/NoOpAuditAdapter.java
@@ -1,8 +1,5 @@
 package dev.igorbarbosa.worktrainingsystem.identity.application;
 
-import org.springframework.stereotype.Component;
-
-@Component
 class NoOpAuditAdapter implements AuditPort {
 	@Override
 	public void record(AuditRecord record) {
